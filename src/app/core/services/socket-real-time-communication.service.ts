8762b0a4160bcc17416a91ea3fe208799d188c8f
joybs1999 <joybrata007@gmail.com>
2024-10-09 19:37:42 +0530

    https_update

--- src/app/core/services/socket-real-time-communication.service.ts
+++ src/app/core/services/socket-real-time-communication.service.ts
@@ -61,7 +61,8 @@ export class SocketRealTimeCommunicationService {
   async connectWebSocket(): Promise<void> {
     try {
       // Construct the WebSocket URL
-      let wsUrl = `${this.socket_ai_interview}ws`;
+      let wsUrl = `${this.test_url_for_ai_interview}ws`;
+      debugger;
       this.socket = new WebSocket(wsUrl);
 
       // Retrieve user details from local storage (if needed)
@@ -135,7 +136,7 @@ export class SocketRealTimeCommunicationService {
 
   async connectionSilienceDetection(): Promise<void> {
     try {
-      let wsUrl = `${this.socket_ai_interview}silenceDetection`;
+      let wsUrl = `${this.test_url_for_ai_interview}silenceDetection`;
 
       this.connectionSilienceDetectionSocket = new WebSocket(wsUrl);
 
@@ -191,7 +192,7 @@ export class SocketRealTimeCommunicationService {
 
   async clearAudioSegmentSocket(): Promise<void> {
 
-    let wsUrl = `${this.socket_ai_interview}clearAudioSegment`;
+    let wsUrl = `${this.test_url_for_ai_interview}clearAudioSegment`;
 
     this.clearAudioSegment = new WebSocket(wsUrl);
 
